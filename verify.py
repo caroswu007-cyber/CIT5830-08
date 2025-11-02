cc2968244f744906b7d18717d5e3a7b89420d253
caroswu007-cyber <caroswu007@gmail.com>
2025-11-02 12:53:13 +0800

    Add files via upload

---
+++ verify.py
@@ -0,0 +1,31 @@
+from web3 import Web3
+from eth_account.messages import encode_defunct
+import random
+
+
+def sign_challenge(challenge):
+    w3 = Web3()
+
+    # 把下面这一行替换为你的私钥（0x + 64 位十六进制）
+    sk = "0x2636feb609bc58bafc604deb0256ac7c607607e93179b15efbd5f431576a096b"
+
+    acct = w3.eth.account.from_key(sk)
+    signed_message = w3.eth.account.sign_message(challenge, private_key=acct.key)
+    return acct.address, signed_message.signature
+
+
+def verify_sig():
+    # 生成随机挑战并验证恢复地址是否等于签名者地址
+    challenge_bytes = random.randbytes(32)
+    challenge = encode_defunct(challenge_bytes)
+    address, sig = sign_challenge(challenge)
+    w3 = Web3()
+    recovered = w3.eth.account.recover_message(challenge, signature=sig)
+    return recovered == address
+
+
+if __name__ == "__main__":
+    if verify_sig():
+        print("You passed the challenge!")
+    else:
+        print("You failed the challenge!")
